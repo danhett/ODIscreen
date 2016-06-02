d506580089f0dec429536d8e661ab6ed8271bd99
Dan Hett <hellodanhett@gmail.com>
2016-06-02 16:01:48 +0100

    Added all images, still on click

--- js/control.js
+++ js/control.js
@@ -3,8 +3,77 @@ var context = canvas.getContext('2d');
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
 
-var imageObj = new Image();
-imageObj.onload = function() {
-  context.drawImage(imageObj, 0, 0);
-};
-imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+// LEEDS
+var leeds1 = {prefix:"leeds/grabs1/screen2-", count:518};
+var leeds2 = {prefix:"leeds/grabs2/screen2-", count:678};
+var leeds3 = {prefix:"leeds/grabs3/screen2-", count:776};
+var leeds4 = {prefix:"leeds/grabs4/screen2-", count:498};
+var leeds5 = {prefix:"leeds/grabs5/screen2-", count:602};
+
+// LONDON
+var london1 = {prefix:"london/grabs1/screen2-", count:538};
+var london2 = {prefix:"london/grabs2/screen2-", count:502};
+var london3 = {prefix:"london/grabs3/screen1-", count:827};
+
+// MANCHESTER
+var manchester1 = {prefix:"manchester/grabs1/screen2-", count:1980};
+var manchester2 = {prefix:"manchester/grabs2/screen2-", count:877};
+
+// SHEFFIELD
+var sheffield1 = {prefix:"sheffield/grabs1/", count:890};
+var sheffield2 = {prefix:"sheffield/grabs2/", count:684};
+var sheffield3 = {prefix:"sheffield/grabs3/screen2+", count:1463};
+
+var screens = []
+var index = 0;
+var max;
+
+// ON UPDATE
+document.body.onclick = function() {
+  var imageObj = new Image();
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, context.canvas.width, context.canvas.height);
+  };
+
+  imageObj.src = "images/" + screens[index] + ".png";
+
+  if(index < (max-1))
+    index++;
+  else {
+    console.log("done");
+  }
+}
+
+// POPULATE IMAGERY
+populateScreens = function() {
+  populate(leeds1);
+  populate(leeds2);
+  populate(leeds3);
+  populate(leeds4);
+  populate(leeds5);
+  populate(london1);
+  populate(london2);
+  populate(london3);
+  populate(manchester1);
+  populate(manchester2);
+  populate(sheffield1);
+  populate(sheffield2);
+  populate(sheffield3);
+
+  max = screens.length;
+}
+
+populate = function(item) {
+  for(var i = 1; i <= item.count; i++) {
+    screens.push(item.prefix+i);
+  }
+}
+
+// MARK THE START OF THE PROCESS
+markStart = function() {
+
+}
+
+// go.
+this.populateScreens();
+this.markStart();
